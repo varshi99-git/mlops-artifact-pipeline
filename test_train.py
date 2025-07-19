8420f5ef5f813ab73780b43edee7e413b6a79051
varshi99-git <g24ai1049@iitj.ac.in>
2025-07-19 14:44:56 +0530

    mlops final

--- test_train.py
+++ test_train.py
@@ -1,9 +1,11 @@
 import json
+from src.train import load_config, train_model
+from sklearn.datasets import load_iris
 from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
 
 def test_config_loading():
-    config = load_config("config/config.json")
+    config = load_config("C:/Users/saiva/mlops-artifact-pipeline/src/config/config.json")
     assert isinstance(config["C"], float)
     assert isinstance(config["solver"], str)
     assert isinstance(config["max_iter"], int)
@@ -11,7 +13,7 @@ def test_config_loading():
 def test_model_training():
     digits = load_digits()
     X, y = digits.data, digits.target
-    config = load_config("config/config.json")
+    config = load_config("C:/Users/saiva/mlops-artifact-pipeline/src/config/config.json")
     model = train_model(X, y, config)
     assert isinstance(model, LogisticRegression)
     assert hasattr(model, "coef_")
@@ -19,7 +21,7 @@ def test_model_training():
 def test_model_accuracy():
     digits = load_digits()
     X, y = digits.data, digits.target
-    config = load_config("config/config.json")
+    config = load_config("C:/Users/saiva/mlops-artifact-pipeline/src/config/config.json")
     model = train_model(X, y, config)
     score = model.score(X, y)
     assert score > 0.8
